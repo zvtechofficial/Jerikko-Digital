694dfe12ed406609f6903cebc863b751b7ee4ae8
zvtechofficial <zjjv.tech@gmail.com>
2024-12-06 10:35:33 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('contact-btn').addEventListener('click', function () {
+    const phoneNumber = '+639493443122'; // Replace with your phone number
+    window.location.href = `tel:${phoneNumber}`;
+});
